25234da1019f101f6e4fda28a9394a58aef432d1
VerKarp <vrkrpv@gmail.com>
2022-07-19 14:52:40 +0300

    Логика поиска файлов

--- FileHasherTestTask/ViewModels/MainWindowVM.cs
+++ FileHasherTestTask/ViewModels/MainWindowVM.cs
@@ -1,6 +1,7 @@
 ﻿using FileHasherTestTask.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,32 @@ namespace FileHasherTestTask.ViewModels
 {
     internal class MainWindowVM : ViewModel
     {
-        private string _title = "File Hasher";
+        private string _path;
+        private IEnumerable<FileInfo> _files;
+        private FileInfo _selectedFile;
 
-        public string Title
+        public string Path
         {
-            get => _title;
-            set => Set(ref _title, value);
+            get => _path;
+            set
+            {
+                Set(ref _path, value);
+
+                if (Directory.Exists(_path))
+                    Files = new DirectoryInfo(_path).EnumerateFiles().Select(file => new FileInfo(file.FullName));
+            }
+        }
+
+        public IEnumerable<FileInfo> Files
+        {
+            get => _files;
+            set => Set(ref _files, value);
+        }
+
+        public FileInfo SelectedFile
+        {
+            get => _selectedFile;
+            set => Set(ref _selectedFile, value);
         }
 
     }
