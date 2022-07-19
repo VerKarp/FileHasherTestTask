95a4daf850241a3a8934b5c1ce0c645c41c4afaf
VerKarp <vrkrpv@gmail.com>
2022-07-19 14:50:33 +0300

    Разметка и проверка работоспособности VM

---
+++ FileHasherTestTask/ViewModels/MainWindowVM.cs
@@ -0,0 +1,21 @@
+﻿using FileHasherTestTask.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHasherTestTask.ViewModels
+{
+    internal class MainWindowVM : ViewModel
+    {
+        private string _title = "File Hasher";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+
+    }
+}
